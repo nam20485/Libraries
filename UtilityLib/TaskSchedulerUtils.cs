eacff7ddc00c4d54dda6923ab27dd388bc32dd52
Nathan Miller <nmiller217@gmail.com>
2020-05-24 18:07:10 -0700

    stage everything from VS

--- UtilityLib/TaskSchedulerUtils.cs
+++ UtilityLib/TaskSchedulerUtils.cs
@@ -1,69 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Win32.TaskScheduler;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using Microsoft.Win32.TaskScheduler;
 
-namespace Libraries.UtilityLib
-{
-    public class TaskSchedulerUtils
-    {
-        protected const string rootFolder = "\\";
+//namespace Libraries.UtilityLib
+//{
+//    public class TaskSchedulerUtils
+//    {
+//        protected const string rootFolder = "\\";
 
-        public class TaskList : List<Task>   { }
+//        public class TaskList : List<Task>   { }
 
-        public static TaskList GetTasksAroundLastRunTime(TimeSpan time, TimeSpan range)
-        {
-            TaskList found = new TaskList();
+//        public static TaskList GetTasksAroundLastRunTime(TimeSpan time, TimeSpan range)
+//        {
+//            TaskList found = new TaskList();
 
-            TimeSpan begin = time - range;
-            TimeSpan end = time + range;
+//            TimeSpan begin = time - range;
+//            TimeSpan end = time + range;
 
-            foreach (var task in GetAllScheduledTasks())
-            {                
-                if (task.LastRunTime.TimeOfDay.OccursInRange(begin, end))
-                {
-                    found.Add(task);
-                }
-            }
+//            foreach (var task in GetAllScheduledTasks())
+//            {                
+//                if (task.LastRunTime.TimeOfDay.OccursInRange(begin, end))
+//                {
+//                    found.Add(task);
+//                }
+//            }
 
-            return found;
-        }
+//            return found;
+//        }
 
-        public static TaskList GetAllScheduledTasks()
-        {
-            TaskList tasks = new TaskList();
-            GetTasksBelowRootFolder(ref tasks);
-            return tasks;
-        }     
+//        public static TaskList GetAllScheduledTasks()
+//        {
+//            TaskList tasks = new TaskList();
+//            GetTasksBelowRootFolder(ref tasks);
+//            return tasks;
+//        }     
 
-        public static void GetTasksBelowRootFolder(ref TaskList tasks)
-        {
-            GetTasksBelowFolder(ref tasks, rootFolder);
-        }
+//        public static void GetTasksBelowRootFolder(ref TaskList tasks)
+//        {
+//            GetTasksBelowFolder(ref tasks, rootFolder);
+//        }
 
-        public static void GetTasksBelowFolder(ref TaskList tasks, string root)
-        {
-            using (var taskService = new TaskService())
-            {
-                TaskFolder rootFolder = taskService.GetFolder(root);
-                if (rootFolder != null)
-                {
-                    GetTasksBelowFolder(ref tasks, rootFolder);
-                }
-            }
-        }
+//        public static void GetTasksBelowFolder(ref TaskList tasks, string root)
+//        {
+//            using (var taskService = new TaskService())
+//            {
+//                TaskFolder rootFolder = taskService.GetFolder(root);
+//                if (rootFolder != null)
+//                {
+//                    GetTasksBelowFolder(ref tasks, rootFolder);
+//                }
+//            }
+//        }
 
-        protected static void GetTasksBelowFolder(ref TaskList tasks, TaskFolder root)
-        {
-            // get this folder's tasks
-            tasks.AddRange(root.GetTasks());
+//        protected static void GetTasksBelowFolder(ref TaskList tasks, TaskFolder root)
+//        {
+//            // get this folder's tasks
+//            tasks.AddRange(root.GetTasks());
 
-            // subfolders'
-            foreach (var subFolder in root.SubFolders)
-            {
-                GetTasksBelowFolder(ref tasks, subFolder);
-            }
-        }
-    }
-}
+//            // subfolders'
+//            foreach (var subFolder in root.SubFolders)
+//            {
+//                GetTasksBelowFolder(ref tasks, subFolder);
+//            }
+//        }
+//    }
+//}
---
+++ UtilityLib/Windows/FormEx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using Libraries.UtilityLib.Windows;
+
+
+namespace Libraries.UtilityLib.Windows
+{
+    public partial class FormEx : Form
+    {
+        public FormEx()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            // Test if the About item was selected from the system menu
+            if ((m.Msg == (int) Win32API.WM.SYSCOMMAND) && (m.WParam.ToInt32() == 0x1/*SYSMENU_ABOUT_ID*/))
+            {
+                MessageBox.Show("Custom About Dialog");
+            }
+        }
+    }
+}
