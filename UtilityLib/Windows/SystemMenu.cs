a55f331256d095ae8a8dbf12c77080119cb26dd7
Nathan Miller <nmiller217@gmail.com>
2020-05-24 19:02:32 -0700

    Add static class and extension method for adding items to a Form's system menu

---
+++ UtilityLib/Windows/SystemMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using Libraries.UtilityLib.Windows;
+//using static Libraries.UtilityLib.Windows.Win32API;
+
+namespace Libraries.UtilityLib.Windows
+{
+    public static class SystemMenu
+    {
+        public static bool AddMenuItem(IntPtr hWnd, Win32API.MenuItemInfoType uFlags, uint uIDNewItem, string lpNewItem)
+        {
+            var hSysMenu = Win32API.GetSystemMenu(hWnd, false);
+            if (hSysMenu != IntPtr.Zero)
+            {
+                return Win32API.AppendMenu(hSysMenu, uFlags, uIDNewItem, lpNewItem);
+            }
+
+            return false;
+        }
+
+        public static bool AddSystemMenuItem(this Form form, Win32API.MenuItemInfoType uFlags, uint uIDNewItem, string lpNewItem)
+        { 
+            return AddMenuItem(form.Handle, uFlags, uIDNewItem, lpNewItem);
+        }
+    }
+}
